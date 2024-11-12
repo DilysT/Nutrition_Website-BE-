7c4f5aabf9a1c3980516ce1b76fc61bdd52159c9
DilysT <21522624@gm.uit.edu.vn>
2024-11-12 21:15:30 +0700

    test render deployment v5

--- src/config/database.js
+++ src/config/database.js
@@ -2,9 +2,14 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  uri: process.env.DATABASE_URL,
-  waitForConnections: true,
-  connectionLimit: 10,
-});
+    host: 'bgqlyyiuxy4lagus1gcm-mysql.services.clever-cloud.com',
+    user: 'upzfcb5lazh82ux2',
+    password: 'rbyugNdZHiPQtf0V5pG1',
+    database: 'bgqlyyiuxy4lagus1gcm',
+    port: 3306,
+    waitForConnections: true,
+    connectionLimit: 10,
+  });
+  
 
 module.exports = pool;
--- src/server.js
+++ src/server.js
@@ -30,6 +30,7 @@ app.use('/api/auth', authenticateToken, reportroutes);
 app.use('/api/auth', authenticateToken, settingroutes);
 app.use('/api/auth', authenticateToken, dashboardroutes);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.listen(port, hostname, () => {
-    console.log(`Server is running on http://0.0.0.0:${port}`);
-  });
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
