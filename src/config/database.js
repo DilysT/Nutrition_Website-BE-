147aa0e9563edb3e570f40784d7e930db359937e
DilysT <21522624@gm.uit.edu.vn>
2024-11-12 16:19:20 +0700

    change in  file database.js

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,10 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
 
-// Create a connection pool
 const pool = mysql.createPool({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'Nopass@21122003',
-    database: 'nutritiondb',
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+  uri: process.env.DATABASE_URL,
+  waitForConnections: true,
+  connectionLimit: 10,
 });
 
 module.exports = pool;
